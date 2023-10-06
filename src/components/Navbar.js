b1f94caadba5020003b74fbb0facbb75b0665c63
Pro-Programmer38 <khursheedahmed786k@gmail.com>
2023-10-06 09:41:21 +0500

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar(props) {
         <nav className="navbar navbar-expand-lg navbar-light bg-purple" style={{ backgroundColor: props.themeColor, fontFamily: props.font }} >
             <a className="navbar-brand" href="http://localhost:3000/"
                 onClick={(event) => handleTabClick(event, 'home')}>
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" className="text-purple-700 text-xl cursor-pointer dark:text-purple-300" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M946.5 505L534.6 93.4a31.93 31.93 0 0 0-45.2 0L77.5 505c-12 12-18.8 28.3-18.8 45.3 0 35.3 28.7 64 64 64h43.4V908c0 17.7 14.3 32 32 32H448V716h112v224h265.9c17.7 0 32-14.3 32-32V614.3h43.4c17 0 33.3-6.7 45.3-18.8 24.9-25 24.9-65.5-.1-90.5z"></path></svg>
+                <svg stroke="currentColor" fill="currentColor" viewBox="0 0 1024 1024" className="text-purple-700 text-xl cursor-pointer dark:text-purple-300" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M946.5 505L534.6 93.4a31.93 31.93 0 0 0-45.2 0L77.5 505c-12 12-18.8 28.3-18.8 45.3 0 35.3 28.7 64 64 64h43.4V908c0 17.7 14.3 32 32 32H448V716h112v224h265.9c17.7 0 32-14.3 32-32V614.3h43.4c17 0 33.3-6.7 45.3-18.8 24.9-25 24.9-65.5-.1-90.5z"></path></svg>
             </a>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ marginLeft: "2rem" }}>
